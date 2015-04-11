f59310a14cb9bc5527d5df6494040be29cd747f6
Daniel Pope <danielthepope@users.noreply.github.com>
2015-04-11 13:44:43 +0100

    Azure ports

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var port = process.env.PORT || 1337;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -25,6 +26,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
