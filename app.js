ad86850195024a44e30cbddee7182597baef2b33
Daniel Pope <danielthepope@users.noreply.github.com>
2016-01-21 22:26:32 +0000

    New users receive the current colour
    Also added favicon and made the footer transparent

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 var port = process.env.PORT || 1337;
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var currentColour = '#FFFFFF';
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+app.use(express.static('public'));
 
-app.get('/:doc', function(req, res) {
-	res.sendfile(req.params.doc);
-});
-
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('a user connected');
+  console.log('current colour is ' + currentColour);
+  socket.emit('update colour', currentColour);
 
-
-  socket.on('send colour', function(colour){
+  socket.on('send colour', function (colour) {
     console.log('new colour: ', colour);
+    currentColour = colour;
     socket.broadcast.emit('update colour', colour);
   });
 
-
-
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     console.log('user disconnected');
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
